203e77bc8e55dbcac0dfdce2b463410d02ebf554
qodIIbop <barnav12@gmail.com>
2016-02-22 08:31:25 +0100

    fixed wordlettersumarray function v2

--- main.c
+++ main.c
@@ -2,11 +2,12 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <ctype.h>
+#include <errno.h>
 
 
-int space=0, word=0, nl=0, letter=0,letternumsum[1000],wordletternum=0;
+int space=0, word=0, nl=0, letter=0,letternumsum[50],wordletternum=0,i=0;
 char actual_char=' ';
-
+//if the actual char is a space returns with 1, if not returns with 0
 int ifspace()
 {
     if(actual_char==' ')
@@ -18,7 +19,7 @@ int ifspace()
         return 0;
     }
 }
-
+//if the actual char is a new line char returns with 1, if not returns with 0
 int ifnl()
 {
     if(actual_char=='\n')
@@ -30,17 +31,16 @@ int ifnl()
         return 0;
     }
 }
-//array not working correctly need to fix so wordletternum is filled into correct array slot!!
+//fill an array with how many words are in the text with that number of letters
 void lettersumarrayfiller()
 {
-    int i,counter;
-    for(i=0;i<1000;i++)
+    for(i=1;i<50;i++)
     {
         if(i==wordletternum)
         {
-            counter++;
+            letternumsum[i]=letternumsum[i]+1;
+            break;
         }
-        i++;
     }
 }
 int main()
@@ -49,10 +49,11 @@ int main()
     char previous_char=' ';
     FILE *txt;
     txt=fopen("test","r");
-/*    if(txt==0)
+    if(txt==0)
     {
-        printf("%d",errno);  //there is no errno global variable
-    }*/
+        printf("errno=%d",errno);
+        return 0;
+    }
     while((actual_char=fgetc(txt))!=EOF)
     {
         if(isalpha(actual_char))
@@ -80,7 +81,10 @@ int main()
         if((actual_char==' ' || actual_char=='\n') && isalpha(previous_char))
         {
             inword_toggle=0;
-            printf("%d\n",wordletternum);
+//            printf("%d\n",wordletternum);
+            lettersumarrayfiller();
+//            printf("%d=%d\n",i,letternumsum[i]);
+
         }
         //adds one to wordletternum value if still in word or resets counter if not
         if(inword_toggle==1)
@@ -93,11 +97,19 @@ int main()
         }
         previous_char=actual_char;
     }
+    lettersumarrayfiller();
     fclose(txt);
-    printf("\n");
     printf("The number of letters in the text is:%d\n",letter);
     printf("The number of words is:%d\n",word);
     printf("The number of spaces is:%d\n",space);
     printf("The number of new lines is:%d\n",nl);
+    printf("\n\n");
+    for(i=0;i<50;i++)
+    {
+        if(letternumsum[i]!='\0')
+        {
+            printf("The number of %d letter words is=%d\n",i,letternumsum[i]);
+        }
+    }
     return 0;
 }
